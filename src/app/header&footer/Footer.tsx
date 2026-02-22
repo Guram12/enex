ad1b83738546f0f121b2cf368476094e78288403
guram12 <guram.shanidze.33@gmail.com>
2026-02-22 20:50:49 +0400

    feat: add Footer component and styles

---
+++ src/app/header&footer/Footer.tsx
@@ -0,0 +1,18 @@
+
+
+
+
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <p>Copyright © 2024 ENEX. All rights reserved.</p>
+    </footer>
+  );
+}
+
+
+
+
+
+
