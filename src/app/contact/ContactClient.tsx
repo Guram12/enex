cd198670afb1898d8735d4d08955996249629f7f
guram12 <guram.shanidze.33@gmail.com>
2026-02-22 23:02:46 +0400

    feat: enhance Contact page layout with responsive design and image integration

--- src/app/contact/ContactClient.tsx
+++ src/app/contact/ContactClient.tsx
@@ -1,11 +1,31 @@
 
 import styles from './Contact.module.css';
-
+import contact_img from '../assets/contact.jpg'
 
 export default function ContactClient() {
+
+
+
   return (
     <div className={styles.contact_container} >
-      <h1>Contact Us</h1>
+      <div className={styles.contact_inside_cont} >
+
+        <div className={styles.image_cont} >
+          <img src={contact_img.src} alt="contact" className={styles.contact_image} />
+        </div>
+
+
+        <div className={styles.contact_form} >
+          <h2 className={styles.contact_title}>Contact Us</h2>
+          <form className={styles.form} >
+            <input type="text" placeholder="Your Name" className={styles.input} />
+            <input type="email" placeholder="Your Email" className={styles.input} />
+            <textarea placeholder="Your Message" className={styles.textarea} />
+            <button type="submit" className={styles.submit_button}>Send Message</button>
+          </form>
+        </div>
+
+      </div>
     </div>
 
 
