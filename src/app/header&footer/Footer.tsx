e89030e65f5a84d232cb06baef4ec697ca3bff56
guram12 <guram.shanidze.33@gmail.com>
2026-02-22 22:19:33 +0400

    feat: add Footer component with styling and integrate into MainPageClient

--- src/app/header&footer/Footer.tsx
+++ src/app/header&footer/Footer.tsx
@@ -1,18 +1,24 @@
-
-
-
-
+import styles from './Footer.module.css'
 
 export default function Footer() {
   return (
-    <footer className="footer">
-      <p>Copyright © 2024 ENEX. All rights reserved.</p>
+    <footer className={styles.footer}>
+
+      <div className={styles.footer_section}>
+        <span className={styles.footer_logo}>ENEX</span>
+      </div>
+
+      <div className={styles.footer_section}>
+        <p className={styles.footer_contact_title}>Contact</p>
+        <p className={styles.footer_text}>📞 551 40 40 30</p>
+        <p className={styles.footer_text}>✉️ info@enex.com</p>
+      </div>
+
+      <div className={styles.footer_divider} />
+      <p className={styles.footer_copyright}>
+        Copyright © 2024 ENEX. All rights reserved.
+      </p>
+
     </footer>
   );
 }
-
-
-
-
-
-
