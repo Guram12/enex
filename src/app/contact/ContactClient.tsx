2ddc2be07de22d2716357774a11ff8d1ac65c1ee
guram12 <guram.shanidze.33@gmail.com>
2026-02-22 22:35:48 +0400

    feat: add Contact module with styling and components for Contact page

---
+++ src/app/contact/ContactClient.tsx
@@ -0,0 +1,30 @@
+
+import styles from './Contact.module.css';
+
+
+export default function ContactClient() {
+  return (
+    <div className={styles.contact_container} >
+      <h1>Contact Us</h1>
+    </div>
+
+
+  )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,24 @@
+
+
+
+import ContactClient from "./ContactClient"
+
+
+
+
+
+
+export default function ContactPage() {
+  return (
+    <>
+      <ContactClient />
+    </>
+  )
+
+}
+
+
+
+
+
+
